35e398407f4285010988cf530c62cadcd1dd2811
zexhior <herizoras1@gmail.com>
2025-01-08 20:16:26 +0300

    [etudes] : faire en sorte que la page soit responsive

--- src/app/etudes/page.tsx
+++ src/app/etudes/page.tsx
@@ -59,25 +59,27 @@ const EtudePage = () => {
 
   return (
     <div
-      className="w-full flex flex-col pb-10 sm:pb-0 sm:flex-row px-2 gap-5 bg-[url('/background.svg')] bg-cover md:h-screen md:overflow-hidden"
+      className="w-full flex flex-col pb-10 lg:pb-0 sm:flex-row px-2 gap-5 bg-[url('/background.svg')] bg-cover lg:h-screen md:overflow-hidden"
       ref={container}
     >
-      <div className="w-full sm:w-1/2 py-20 sm:py-0 px-6 flex flex-col justify-center items-start">
-        <h3 className="font-bold text-3xl py-3">Formations</h3>
-        <p className="text-lg">
-          J'ai eu l'opportunité de passer mes 5 merveilleurses années d'étude à
-          l'Institut Supérieur Polytechnique de Madagascar. Suivant le système
-          LMD, j'ai pu à la fois faire de recherche personnel en étant supervisé
-          par les professeurs à l'université. J'ai pu aiguisé mes compétences
-          dans la résolution de problème complexe{" "}
-        </p>
-        <img
-          src="/logo/logo_ispm.png"
-          className="rounded-full"
-          alt="ISPM"
-          width={"300px"}
-          height={"300px"}
-        />
+      <div className="w-full sm:w-1/2 py-20 sm:py-0 px-6 flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-start pt-20">
+          <h3 className="font-bold text-3xl py-3">Formations</h3>
+          <p className="text-lg">
+            J'ai eu l'opportunité de passer mes 5 merveilleurses années d'étude
+            à l'Institut Supérieur Polytechnique de Madagascar. Suivant le
+            système LMD, j'ai pu à la fois faire de recherche personnel en étant
+            supervisé par les professeurs à l'université. J'ai pu aiguisé mes
+            compétences dans la résolution de problème complexe{" "}
+          </p>
+          <img
+            src="/logo/logo_ispm.png"
+            className="rounded-full"
+            alt="ISPM"
+            width={"300px"}
+            height={"300px"}
+          />
+        </div>
       </div>
       <div className="w-full sm:w-1/2 flex flex-col sm:justify-center items-end gap-3">
         {formations.map((formation: Formation, index: number) => {
